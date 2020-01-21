9869dab94b7f17b60316944eab6eac415084596f
Juan <juananguillo@gmail.com>
2020-01-21 20:35:48 +0100

    Version Comentada

--- src/Rectangulo_JAGL.java
+++ src/Rectangulo_JAGL.java
@@ -1,22 +1,51 @@
+/**
+
+ * @author: Juan Antonio Guillo Lopez
+
+ * @version: 21/01/2020
+ * Clase que contiene una clase abstracta
+ 
+ */ 
+
 
 public class Rectangulo_JAGL extends FiguraGeometrica_JAGL {
+	/* Clase Rectangulo
+	 *  Campos de clase
+	 */
 	private double l1;
 	private double l2;
+	
+	/**
+	 *  
+	     * Constructor de la clase Rectangulo
+	     *  @param tipoFigura que es una constante de una clase abstracta
+
+	     */ 
+	
 	
 	public Rectangulo_JAGL(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
-	}
-
+	}// Cierre del constructor
+	
+	
+	/* 
+	 * Metodo que devuelve el perimetro del rectangulo sumando los lados
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
 	
+
+	
+	/* 
+	 * Metodo que devuelve el perimetro del rectangulo sumando los lados
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
 	}
 
-}
+}// Fin de la clase

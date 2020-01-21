20da74c51e8c774449f81b9d457a578dec9daa9d
Juan <juananguillo@gmail.com>
2020-01-21 19:56:11 +0100

    Version refactorizada

--- src/FiguraGeometrica_JAGL.java
+++ src/FiguraGeometrica_JAGL.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_JAGL {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_JAGL(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
--- src/Rectangulo_JAGL.java
+++ src/Rectangulo_JAGL.java
@@ -1,9 +1,9 @@
 
-public class Rectangulo extends FiguraGeometrica {
+public class Rectangulo_JAGL extends FiguraGeometrica_JAGL {
 	private double l1;
 	private double l2;
 	
-	public Rectangulo(String tipoFigura, double lG, double lP) {
+	public Rectangulo_JAGL(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
